fc1a9f8b03ffbceb83b4dfd1a91cadf1f9babdb2
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-04-28 07:17:06 +0200

    working in api.js

---
+++ js/api.js
@@ -0,0 +1,23 @@
+export async function resolverDudas(historial) {
+    
+    try {
+        const respuesta = await fetch("https://api.anthropic.com/v1/messages", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                model: "claude-sonnet-4-20250514",
+                max_tokens: 1024,
+                messages: historial  // ← array con el historial de mensajes
+            })
+        })
+        const datos = await respuesta.json()
+        return datos.content[0].text
+        
+    } catch (error) {
+        return null
+    }
+    
+    
+}
